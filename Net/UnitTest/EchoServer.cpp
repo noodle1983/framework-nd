751305eb1c2c42cda891bf2a85261484a1323dde
LuDong <noodle1983@126.com>
2011-09-19 15:21:47 +0800

    handle siginal

--- Net/UnitTest/EchoServer.cpp
+++ Net/UnitTest/EchoServer.cpp
@@ -8,10 +8,34 @@
 #include <unistd.h>
 #include <signal.h>
 
+static int closed = false;
+static boost::mutex closedMutexM;
+static boost::condition_variable closedCondM;
+void sigint(int)
+{
+    printf("stopping...\n");
+    boost::lock_guard<boost::mutex> lock(closedMutexM);
+    closed = true;
+    closedCondM.notify_one();
+}
+
 int main()
 {
+    pid_t pid = fork();
+    if (pid < 0)
+    {
+        printf("failed to fork!\n");
+        return -1;
+    }
+    else if (pid > 0)
+    {
+        printf("server start with pid %d.\n", pid);
+        return 0;
+    }
+
     signal(SIGPIPE, SIG_IGN);
     signal(SIGALRM, SIG_IGN);
+    signal(SIGINT, sigint);
     evthread_use_pthreads();
     Processor::BoostProcessor processor(4);
     processor.start();
@@ -20,8 +44,15 @@ int main()
     Net::Server::TcpServer server(&echoProtocol, &reactor, &processor);
     server.startAt(5555);
     reactor.start();
-	while(1){sleep(1);};
+    
+    boost::unique_lock<boost::mutex> lock(closedMutexM);
+	while(!closed)
+    {
+        closedCondM.wait(lock);
+    }
+
     processor.stop();
+    reactor.stop();
     return 0;
 }
 
